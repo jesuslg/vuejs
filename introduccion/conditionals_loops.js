03daa148a04508dcc51d8e2b8b3887b305fea7c3
Jesús LG <jesuslg@e-quipu.pe>
2018-02-01 17:20:36 -0500

    2da parte hecha

---
+++ introduccion/conditionals_loops.js
@@ -0,0 +1,14 @@
+var app3 = new Vue({
+    el: "#app3",
+    data: {
+        seen: true, //boolean
+        frutas: ['manzana', 'pera', 'plátano', 'papaya'] //array
+    }
+})
+/**
+ * El "seen" como es una variable puede ser string, boolean o numérico
+ * si usas la consola y haces esto epp3.seen = false , verás q gracias 
+ * a la interpolación verás que es lo que sucede.
+ * 
+ * Por otro lado ves que fruta es un array asi que ya sabes no seas gil! :v 
+ */
