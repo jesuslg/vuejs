017ad2d8d7e13f2ff138ac53d2c858a58b4fe5d7
Jesús LG <jesuslg@e-quipu.pe>
2018-02-02 16:38:10 -0500

    comienzo de the vue instance

---
+++ n002_the_vue_instance/creating_a_vue_instance.js
@@ -0,0 +1,21 @@
+/**
+ * Acuérdate que todas la app Vue se inician instanciando
+ * el objeto Vue
+ */
+var vm = new Vue({
+
+})
+/**
+ * 
+ * Los grandes proyectos puede desplegarse así
+ * 
+Root Instance
+└─ TodoList
+   ├─ TodoItem
+   │  ├─ DeleteTodoButton
+   │  └─ EditTodoButton
+   └─ TodoListFooter
+      ├─ ClearTodosButton
+      └─ TodoListStatistics
+ */
+
