4553bb0c770db553db2657aa90c44a91776a26f7
Jesús LG <jesuslg@e-quipu.pe>
2018-02-05 12:49:25 -0500

    n02 data y métodos

--- n002_the_vue_instance/n02_data_and_methods.js
+++ n002_the_vue_instance/n02_data_and_methods.js
@@ -28,8 +28,8 @@ console.log("---Ejemplo4---");
 datos.a = 3; // reasignamos de otra manera y que crees?
 console.log(vm.a);// pues si de la otra forma para obtener mostró el dato cambiado
 console.log("---Ejemplo5---");
-vm.b = 'h1';
-console.log(vm.b);
+// vm.b = 'h1';
+// console.log(vm.b);
 /**
  * 
  * Este ultimo ejemplo es bien interesante si uno pone de esta manera , esta bien
@@ -46,3 +46,45 @@ console.log(vm.b);
     error: null
  * }
  */
+ var obj = {
+     foo: 'bar'
+ }
+// Object.freeze(obj); //Esto lo que hace es freezear es decir congela las propiedades de tu objeto
+                    //es decir q no puedes agregar más, no puedes editarlo ni nada.
+//Prueba 1
+// new Vue({
+//     el: '#appmini',
+//     data (){
+//         return obj;
+//     }
+// })
+//Prueba 2
+new Vue({
+    el: '#appmini',
+    data () {
+      return {
+        obj
+      }
+    }
+  })
+/**
+ * Si quieres intenta reasignar y verás q no podrás
+ */
+// obj.b = 49
+// obj.foo = 100
+console.log(obj.foo);
+
+//para el appmini 2
+var example = {a:1}
+var vuem = new Vue({
+  el: "#appmini2",
+  data: example
+})
+
+console.log(vuem.$data) // es igual a la example
+console.log(vuem.$el) // capturas el DOM document.getElementById('example'); 
+vuem.$watch('a', function(newV, oldVal){
+  console.log(`Nuevo : ${newV} - Viejo: ${oldVal}`);
+})
+//Con este ultimo $watch es cuando se manipula valores 
+//y la misma lógica en si va en el html y aca hago lo q quiera dentro de la funcion
