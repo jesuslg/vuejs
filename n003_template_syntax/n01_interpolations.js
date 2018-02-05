786967eadc37a7d3f0430007efe74916f398cad9
Jesús LG <jesuslg@e-quipu.pe>
2018-02-05 16:28:58 -0500

    create template syntax

---
+++ n003_template_syntax/n01_interpolations.js
@@ -0,0 +1,38 @@
+/**
+ * #Text
+ */
+new Vue({
+    el: '#parte1',
+    data:{
+        parte1: 'Holi es parte 1'
+    }
+})
+ /**
+ * #Raw HTML
+ */
+new Vue({
+    el: '#parte2',
+    data:{
+        parte2: '<span style="color:red;">Holi es parte 2 con color</span>'
+    }
+})
+ /**
+ * #Attributes
+ */
+
+new Vue({
+    el: '#parte3',
+    data:{
+        parte3: true
+    }
+})
+/**
+ * #Using Javascript expressions
+ */
+new Vue({
+    el: '#parte4',
+    data: {
+        parte4:5
+    }
+})
+
